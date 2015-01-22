fff0bfce1c5b1a9e1762e4ef2d5656f9cfa2f9c2
Nathan Hirata <nathanhirata@yahoo.com>
2015-01-22 15:01:24 -0800

    new comments

--- public_html/public_html/script.js
+++ public_html/public_html/script.js
@@ -1,48 +1,50 @@
-$("document").ready(function(){
-    
-    $(".yeah").css("background-color", "darkblue");
-    
+$("document").ready(function() {
+
+    $(".yeah").css("background-color", "silver");
+
     $(".projectColor").css("background-color", "lightblue");
-    
+
     $(".k").css("color", "white");
-    
-    
-    
-    
-    
+
+
+
+//this is code to activate my buttons
+
     $('replaceWText').bind('click', replaceWText);
-    
+
     $('#slideAway').bind('click', slideTheImage);
-    
+
     $('#showLogo').bind('click', showTheImage);
-    
+
     $('#hideLogo').bind('click', hideTheImage);
-    
+
     $('#showLogov2').bind('click', showTheImagev2);
-    
+
     $('#hideLogov2').bind('click', hideTheImagev2);
-    
-    
-    
-    
+
+
+
+//this is my accordion for my links
     $('#superHumans').accordion({header: "h3"});
 
 });
 
+
+//these are the functions for my buttons
 function slideTheImage() {
     $('#logo').slideUp(2500);
 }
 
 function showTheImage() {
-    $('#logo').show('fold', {},2500);
+    $('#logo').show('fold', {}, 2500);
 }
 
 function hideTheImage() {
-    $('#logo').hide('fold', {},2500);
+    $('#logo').hide('fold', {}, 2500);
 }
 
 function showTheImagev2() {
-    $('#logo').show('explode', {},2500);
+    $('#logo').show('explode', {}, 2500);
 }
 
 function replaceWText() {
@@ -50,18 +52,18 @@ function replaceWText() {
 }
 
 function hideTheImagev2() {
-    $('#logo').hide('explode', {},2500);
+    $('#logo').hide('explode', {}, 2500);
 }
 
-$("document").ready(function(){
-    $(".red").css("background-color", "lime");
+$("document").ready(function() {
+    $(".red").css("background-color", "darkblue");
 });
 
-$("document").ready(function(){
+$("document").ready(function() {
     $(".yeah").css("color", "white");
 });
 
-$("document").ready(function(){
+$("document").ready(function() {
     $('.no').bind('click', alertButtonClick);
 });
 
@@ -69,24 +71,15 @@ function alertButtonClick() {
     alert("SIKE THATS THE WRONG NUMBER");
 }
 
-$("document").ready(function(){
-    
+//this is the fucntion when i hover over the phone number at the top of my jquery page 
+
+$("document").ready(function() {
+
     $("h1").bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe);
-    
+
     $('h1').bind('click', mouseClick);
 });
 
 function mouseOverMe() {
     $("h1").html("SIKE THATS THE WRONG NUMBER");
 }
-
-function mouseOutMe() {
-    $('h1').html('SIKE THATS THE WRONG NUMBER');
-}
-
-function mouseClick() {
-    $('p').html('Harder Johnny');
-}
-
-
-
